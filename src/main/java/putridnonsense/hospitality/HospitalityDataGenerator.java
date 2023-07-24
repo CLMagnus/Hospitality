f56d3e8fa8cd29642fb3cf1e53e36c533bb17561
putrid-nonsense <thebetterclmagnus@gmail.com>
2023-07-25 00:46:47 +0100

    Start on data gen will start porting soon

--- src/main/java/putridnonsense/hospitality/HospitalityDataGenerator.java
+++ src/main/java/putridnonsense/hospitality/HospitalityDataGenerator.java
@@ -2,10 +2,18 @@ package putridnonsense.hospitality;
 
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+import net.minecraft.registry.RegistryBuilder;
+import putridnonsense.hospitality.data.ModWorldGenerator;
 
 public class HospitalityDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+		FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
 
+		pack.addProvider(ModWorldGenerator::new);
+	}
+
+	@Override
+	public void buildRegistry(RegistryBuilder registryBuilder){
 	}
 }
